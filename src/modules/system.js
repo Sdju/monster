90a94b2d101db7f31cd0da62174135bdf5b5d29e
zede <zede@ventumit.ru>
2019-12-05 00:15:18 +0500

    create samples usage API

--- src/modules/system.js
+++ src/modules/system.js
@@ -7,7 +7,12 @@ const systemModule = new Module();
 systemModule.addCommand(new Command({
     name: 'ping',
     aliases: ['пинг'],
-    meta: {},
+    meta: {
+        help: {
+            description: 'Текущий пинг до сервера',
+            sample: 'ping'
+        }
+    },
     forms: {
         answer({ ping }) {
             return {
@@ -19,6 +24,52 @@ systemModule.addCommand(new Command({
     },
     async handler() {
         await this.answer({ping: 500});
+    },
+}));
+
+systemModule.addCommand(new Command({
+    name: 'invite',
+    aliases: ['инвайт'],
+    meta: {
+        help: {
+            description: 'Отправляет ссылку на приглашение бота на адмиинистрируемые вами сервера',
+            sample: 'invite'
+        }
+    },
+    forms: {
+        answer({ invite }) {
+            return {
+                color: 0x3498DB,
+                title: `:link: Приглашение бота на другие сервера`,
+                description: `[${invite}](${invite})`,
+            }
+        },
+    },
+    async handler() {
+        await this.answer({invite: await this.module.processor.client.generateInvite(8)});
+    },
+}));
+
+systemModule.addCommand(new Command({
+    name: 'waiter',
+    meta: {},
+    forms: {
+        answer() {
+            return {
+                color: 0x3498DB,
+                title: `:link: Ждун`,
+                description: `ыыыы`,
+            }
+        },
+    },
+    async handler() {
+        await this.answer({});
+        await this.hookContainer.waitHook(this.module, 'message', (message)=> {
+            if (message.author.id === this.module.processor.client.user.id) {
+                return true;
+            }
+        });
+        console.log('ДОЖДАЛСЯ');
     },
 }));
 
